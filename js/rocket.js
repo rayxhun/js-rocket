fabd4c83de2a1354aa97cdc3b4879d51f6a7f7bf
rayxhun <rayxhun@gmail.com>
2019-10-11 16:39:47 +0200

    Update rocket.js

--- js/rocket.js
+++ js/rocket.js
@@ -1,3 +1,15 @@
+var timer = 0;
+var countdownNumber = 10;
+
 var changeState = function (state) {
  document.body.className = 'body-state' + state;
+
+ if (state == 2) {
+ 	timer = setInterval(function () {
+ 		document.getElementById('countdown').innerHTML =
+ 		 countdownNumber;
+ 		 countdownNumber = countdownNumber 
+ 		 -1;
+ 		} , 1000);
+ };
 }
